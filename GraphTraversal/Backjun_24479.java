209c62e9a82a9089d1db0ca0b61abed7ecbec18b
kim.daehwan1 <kim.daehwan1@joins.com>
2025-09-25 11:07:29 +0900

    feat : Backjun24479, Backjun24480

---
+++ GraphTraversal/Backjun_24479.java
@@ -0,0 +1,67 @@
+package GraphTraversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Backjun_24479 {
+    
+    static ArrayList<Integer>[] graph;
+    static int[] visited;
+    static int order = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 정점 수
+        int M = Integer.parseInt(st.nextToken()); // 간선 수
+        int R = Integer.parseInt(st.nextToken()); // 시작 정점
+
+        graph = new ArrayList[N + 1];
+        visited = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 간선 입력
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        // 오름차순 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        // DFS 시작
+        dfs(R);
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void dfs(int node) {
+        visited[node] = order++; // 방문 순서 기록
+
+        for (int next : graph[node]) {
+            if (visited[next] == 0) { // 아직 방문 안 했으면
+                dfs(next);
+            }
+        }
+    }
+
+}
